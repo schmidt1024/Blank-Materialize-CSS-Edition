421d94bcaa9535a60ca861319081728139cf6455
Alexander Schmidt <alexander.schmidt@edvas.de>
2015-02-18 20:42:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# [Blank Materialize CSS Edition](http://blank.vc)
+
+Light, powerful and free template for Joomla!
+for faster and easier web development.
+
+## Functions
+
+* Develop with bootstrap for responsive web design
+* Scalable and customizable vector icons
+* Customizable error, offline and print page
+
+## Addons 
+
+* [Materialize 0.95.2](http://materializecss.com/)
+
+## PSD
+
+[Download psd.zip](http://itr.im/psd) (12 MB) for faster icon and thumbnail creation. Package inlcudes following files:
+
+* App Icon Template [2.0].psd
+* template_preview.psd
+* template_thumbnail.psd
+
+## Blank
+
+Take a look at [Blank without Bootstrap](https://github.com/Bloggerschmidt/Blank).
+
+## Licence
+
+This template is opensource software released under the [GPL](http://www.gnu.org/licenses/gpl-2.0.txt).
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php defined( '_JEXEC' ) or die; 
+
+// variables
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument(); 
+$menu = $app->getMenu();
+$active = $app->getMenu()->getActive();
+$params = $app->getParams();
+$pageclass = $params->get('pageclass_sfx');
+$tpath = $this->baseurl.'/templates/'.$this->template;
+$templateparams	= $app->getTemplate(true)->params;
+
+// generator tag
+$this->setGenerator(null);
+
+// force latest IE & chrome frame
+$doc->setMetadata('x-ua-compatible','IE=edge,chrome=1');
+
+// js
+JHtml::_('jquery.framework');
+$doc->addScript($tpath.'/js/materialize.min.js');
+$doc->addScript($tpath.'/js/logic.js'); // <- use for custom script
+
+// css
+$doc->addStyleSheet($tpath.'/css/materialize.css');
+$doc->addStyleSheet($tpath.'/css/template.css');
+
+?><!doctype html>
+
+<html lang="<?php echo $this->language; ?>">
+
+<head>
+	<jdoc:include type="head" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+	<link rel="apple-touch-icon-precomposed" href="<?php echo $tpath; ?>/images/apple-touch-icon-57x57-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $tpath; ?>/images/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $tpath; ?>/images/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $tpath; ?>/images/apple-touch-icon-144x144-precomposed.png">
+	<!-- Le HTML5 shim and media query for IE8 support -->
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script type="text/javascript" src="<?php echo $tpath; ?>/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+  
+<body class="<?php echo (($menu->getActive() == $menu->getDefault()) ? ('front') : ('site')).' '.$active->alias.' '.$pageclass; ?>" role="document">
+
+	<!-- 
+		YOUR CODE HERE
+	-->
+
+	<jdoc:include type="modules" name="debug" />
+</body>
+
+</html>
